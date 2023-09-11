b633866e4432a520b7083a953f77c82841c42e3e
smshaown <smshaown65@gmail.com>
2023-09-11 08:23:20 +0600

    added the project cart start

---
+++ src/components/PagesSection/Services/Services1/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import banner from '../../../../assets/images/Pages/Services/Services1/banner.jpg';
+
+const ProjectCard = () => {
+    return (
+        <div className='myContainer relative mt-[-185px]'>
+            <img className='rounded-lg' src={banner} alt="Banner Img" />
+        </div>
+    );
+};
+
+export default ProjectCard;
--- src/components/PagesSection/Services/Services1/Services.jsx
+++ src/components/PagesSection/Services/Services1/Services.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Services1Banner from './Services1Banner';
+import ProjectCard from './ProjectCard';
 
 const Services = () => {
     return (
-        <div className='z-10'>
+        <div className='z-10 mb-80'>
             <Services1Banner />
+            <ProjectCard />
         </div>
     );
 };
