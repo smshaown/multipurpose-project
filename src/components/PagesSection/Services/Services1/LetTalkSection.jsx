124e1caed9d6a39a0ca61f2a2627d832de148b1a
smshaown <smshaown65@gmail.com>
2023-09-15 11:34:32 +0600

    Let's us section start

---
+++ src/components/PagesSection/Services/Services1/LetTalkSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import img1 from '../../../../assets/images/Pages/Services/Services1/g5.jpg';
+import img2 from '../../../../assets/images/Pages/Services/Services1/g6.jpg';
+
+export const LetTalkSection = () => {
+  return (
+    <div>
+        <div>
+            <div>
+              <img src={img1} alt="Let's Talk" />
+            </div>
+            <div>
+                <div className='w-60 h-52 bg-[#E0E9FA]'>
+
+                </div>
+                <div>
+                    <img src={img2} alt="" />
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
