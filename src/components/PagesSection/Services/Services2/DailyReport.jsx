da9c9f674b63de7ab0117e07e474526924c636df
smshaown <smshaown65@gmail.com>
2023-09-15 20:54:10 +0600

    section hook title crect and dily Report added

--- src/components/PagesSection/Services/Services2/DailyReport.jsx
+++ src/components/PagesSection/Services/Services2/DailyReport.jsx
@@ -1,11 +1,17 @@
+import { Title } from "../../../SectionHook/Title"
 
 
 export const DailyReport = () => {
   return (
-    <div className="">
-        <div>
-            <div>
-                <div></div>
+    <div className="mt-40">
+        <div className="myContainer">
+            <div className="flex flex-col md:flex-row justify-between items-center">
+                <div className="w-[495px] md:ml-[34px]">
+                    <Title title="What We Do?" />
+                </div>
+                <div className="">
+
+                </div>
             </div>
         </div>
     </div>
---
+++ src/components/SectionHook/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Title = ({title}) => {
+  return (
+    <h4 className="uppercase text-[#aab0bc] font-bold text-lg tracking-wide">
+    {title}
+  </h4>
+  )
+}
