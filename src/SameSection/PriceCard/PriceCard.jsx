4c9c2f9f9944aa1d22c14212c7d39c1a9904330f
smshaown <smshaown65@gmail.com>
2023-09-15 11:13:52 +0600

    Our Pricing section added the css code

--- src/SameSection/PriceCard/PriceCard.jsx
+++ src/SameSection/PriceCard/PriceCard.jsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { BiCheck } from 'react-icons/bi';
 import { Button } from '../Button/Button';
 
-export const PriceCard = () => {
+export const PriceCard = ({price, yr, api, storage, project}) => {
   return (
-    <div>
-        <header>
-            <h1><span>$</span> <span>199</span><sub>/yr</sub> </h1>
-            <h3>Premium Plan</h3>
+    <div className="mt-10 w-[305px] h-[500px] rounded-lg flex flex-col justify-center items-center z-10 relative" style={{boxShadow: 'rgba(0, 0, 0, 0.15) 0px 5px 15px 0px'}}>
+        <header className=''>
+            <h1 className="text-[#343f52]"><sup className='text-xl font-medium'>$</sup> <span className="text-4xl font-bold">{price}</span><sub className="font-medium text-base">{yr}</sub> </h1>
+            <h3 className="text-[#343f52] text-xl mt-2 mb-6 font-bold">Premium Plan</h3>
         </header>
         <div>
-            <li>< BiCheck /> <span> 100K </span>API Access</li>
-            <li>< BiCheck /> <span> 200MB </span>200MB Storage</li>
-            <li>< BiCheck /> <span> Weekly </span>Reports</li>
-            <li>< BiCheck /> <span> 7/24 </span>Support</li>
+            <ul className='mb-8'>
+                <li className='flex flex-row gap-2 mt-4'>< BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> <span className="font-bold text-black"> {project} </span>Project</li>
+                <li className='flex flex-row gap-2 mt-4'>< BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> <span className="font-bold text-black"> {api} </span>API Access</li>
+                <li className='flex flex-row gap-2 mt-4'>< BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> <span className="font-bold text-black"> {storage} </span>Storage</li>
+                <li className='flex flex-row gap-2 mt-4'>< BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> <span className="font-bold text-black"> Weekly </span>Reports</li>
+                <li className='flex flex-row gap-2 mt-4'>< BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> <span className="font-bold text-black"> 7/24 </span>Support</li>
+            </ul>
         </div>
         <Button button="Choose Plan" />
     </div>
--- src/components/PagesSection/Services/Services1/OurPricing.jsx
+++ src/components/PagesSection/Services/Services1/OurPricing.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button } from "../../../../SameSection/Button/Button";
+import { PriceCard } from "../../../../SameSection/PriceCard/PriceCard";
 
 export const OurPricing = () => {
   const [selectMonthly, setSelectMonthly] = useState(true);
@@ -10,40 +11,73 @@ export const OurPricing = () => {
 
   return (
     <div className="myContainer mt-40">
-      <div className="flex flex-row justify-end items-center gap-4">
-        <span className="text-base font-semibold">Monthly</span>
-        <label className="flex cursor-pointer select-none items-center">
-          <div className="relative">
-            <input
-              type="checkbox"
-              checked={selectMonthly}
-              onChange={handleCheckboxChange}
-              className="sr-only"
-            />
-            <div
-              className={`box block h-7 w-12 rounded-full border border-[#3f78e0] bg-[#3f78e0] ${
-                selectMonthly ? "bg-primary" : "bg-dark"
-              }`}
-            ></div>
-            <div
-              className={`absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#f6f7f9] transition ${
-                selectMonthly ? "translate-x-full" : ""
-              }`}
-            ></div>
-          </div>
-        </label>
-        <h4 className="text-base font-semibold">Yearly <span className="text-red-500">(Save 30%)</span></h4>
-      </div>
-      <div>
-        <div>
-           <h4 className="uppercase text-[#aab0bc] font-bold text-lg tracking-wide">Our Pricing</h4>
-           <h1 className="text-[#343f52] text-[1.8rem] font-bold py-4">We offer great and premium prices.</h1>
-           <p className="text-[#60697b] text-base font-medium pb-4">Enjoy a <span className="text-[#3f78e0]">free 30-day</span> trial and experience the full service. No credit card required!</p>
-           <Button button="See All Prices" />
-            {/* <h2>price={selectMonthly ? "20.99" : "188.9"}</h2> */}
+      <div className="flex flex-col lg:flex-row justify-between mt-20">
+        <div className="mt-20">
+          <h4 className="uppercase text-[#aab0bc] font-bold text-lg tracking-wide">
+            Our Pricing
+          </h4>
+          <h1 className="text-[#343f52] text-6xl font-bold py-4">
+            We offer great and premium prices.
+          </h1>
+          <p className="text-[#60697b] text-base font-medium pb-8 mt-8">
+            Enjoy a <span className="text-[#3f78e0]">free 30-day</span> trial
+            and experience the full service. No credit card required!
+          </p>
+          <Button button="See All Prices" />
+          {/* <h2>price={selectMonthly ? "20.99" : "188.9"}</h2> */}
         </div>
+
         <div>
-            
+          <div className="flex flex-row lg:justify-end items-center gap-4 lg:mt-0 mt-20">
+            <span className="text-base font-semibold">Monthly</span>
+            <label className="flex cursor-pointer select-none items-center">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  checked={selectMonthly}
+                  onChange={handleCheckboxChange}
+                  className="sr-only"
+                />
+                <div
+                  className={`box block h-7 w-12 rounded-full border border-[#3f78e0] bg-[#3f78e0] ${
+                    selectMonthly ? "bg-primary" : "bg-dark"
+                  }`}
+                ></div>
+                <div
+                  className={`absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#f6f7f9] transition ${
+                    selectMonthly ? "translate-x-full" : ""
+                  }`}
+                ></div>
+              </div>
+            </label>
+            <h4 className="text-base font-semibold">
+              Yearly <span className="text-red-500">(Save 30%)</span>
+            </h4>
+          </div>
+          <div className="flex flex-col md:flex-row gap-4 justify-center items-center">
+            <div>
+              <PriceCard
+                className=" z-50"
+                price={selectMonthly ? "199" : "19"}
+                yr={selectMonthly ? "/yr" : "/mo"}
+                project="5"
+                api="100k"
+                storage="200MB"
+              />
+
+              {/* <div className=" w-[160px] h-[160px] bg-[#edf2fc] rounded-full top-0  -z-10"></div> */}
+            </div>
+
+            <div className="h-[600px]">
+              <PriceCard
+                price={selectMonthly ? "499" : "49"}
+                yr={selectMonthly ? "/yr" : "/mo"}
+                project="20"
+                api="300k"
+                storage="500MB"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
