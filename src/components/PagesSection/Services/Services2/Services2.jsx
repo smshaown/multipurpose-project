130c6c8811a2f7f5504ab82b7b8166b87f101817
smshaown <smshaown65@gmail.com>
2023-09-15 23:01:06 +0600

    Daily Report section done

--- src/components/PagesSection/Services/Services2/Services2.jsx
+++ src/components/PagesSection/Services/Services2/Services2.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { BannerSection } from '../../../BannerSection/BannerSection'
+import { DailyReport } from './DailyReport'
 
 export const Services2 = () => {
   return (
     <div>
       <BannerSection />
+      <DailyReport />
     </div>
   )
 }
