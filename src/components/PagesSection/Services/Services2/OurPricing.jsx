40d95ddfce5a1025cdf9fe4f52f8e641beb9d0e5
smshaown <smshaown65@gmail.com>
2023-09-16 13:22:07 +0600

    our price sucess section

--- src/components/PagesSection/Services/Services2/OurPricing.jsx
+++ src/components/PagesSection/Services/Services2/OurPricing.jsx
@@ -1,22 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Title } from "../../../../SameSection/Title/Title";
+import { OurPricingCard } from "./OurPricingCard";
+import "../services.css";
 
 export const OurPricing = () => {
+  const [selectMonthly, setSelectMonthly] = useState(true);
+
+  const handleCheckboxChange = () => {
+    setSelectMonthly((prev) => !prev);
+  };
+
   return (
-    <div className="mt-20 md:mt-40 bg-[#EDF2FC] w-full h-[580px] mb-40">
-      <div className="myContainer relative">
+    <div className="mt-20 md:mt-40 bg-[#EDF2FC] w-full h-[600px] mb-40">
+      <div className="myContainer">
         <div className="w-full lg:w-[860px] flex flex-col mx-auto justify-center items-center pt-[128px]">
-        <span>
+          <span>
             <Title title="Our Process" />{" "}
           </span>
           <h1 className="text-[#343f52] text-2xl md:text-4xl font-bold text-center py-4">
-              We offer great prices, premium products and quality service for your business.
+            We offer great prices, premium products and quality service for your
+            business.
           </h1>
         </div>
 
+        <div className="">
+          <div className="flex flex-row justify-center items-center gap-4 mt-20">
+            <span className="text-base font-semibold">Monthly</span>
+            <label className="flex cursor-pointer select-none items-center">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  checked={selectMonthly}
+                  onChange={handleCheckboxChange}
+                  className="sr-only"
+                />
+                <div
+                  className={`box block h-7 w-12 rounded-full border border-[#3f78e0] bg-[#3f78e0] ${
+                    selectMonthly ? "bg-primary" : "bg-dark"
+                  }`}
+                ></div>
+                <div
+                  className={`absolute left-1 top-1 flex h-5 w-5 items-center justify-center rounded-full bg-[#f6f7f9] transition ${
+                    selectMonthly ? "translate-x-full" : ""
+                  }`}
+                ></div>
+              </div>
+            </label>
+            <h4 className="text-base font-semibold">
+              Yearly <span className="text-red-500">(Save 30%)</span>
+            </h4>
+          </div>
+        </div>
 
-        <div className="absolute">
-            
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-center ourPriceCard">
+          <OurPricingCard
+            className=" z-50"
+            price={selectMonthly ? "199" : "19"}
+            yr={selectMonthly ? "/yr" : "/mo"}
+            project="5"
+            api="100k"
+            storage="200MB"
+          />
+
+          <OurPricingCard
+            price={selectMonthly ? "499" : "49"}
+            yr={selectMonthly ? "/yr" : "/mo"}
+            project="20"
+            api="300k"
+            storage="500MB"
+          />
+
+          <OurPricingCard 
+            price={selectMonthly ? "199" : "19"}
+            yr={selectMonthly ? "/yr" : "/mo"}
+            project="5"
+            api="100k"
+            storage="200MB"
+          />
         </div>
       </div>
     </div>
---
+++ src/components/PagesSection/Services/Services2/OurPricingCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { BiCheck } from "react-icons/bi";
+import { Button } from "../../../../SameSection/Button/Button";
+import "../Services.css";
+
+export const OurPricingCard = ({ price, yr, api, storage, project }) => {
+  return (
+    <div
+      className="mt-10 w-full md:w-[350px]  md:gap-4 lg:gap-0 rounded-lg flex flex-col justify-center items-center z-10 relative bg-white  mb-20 ourPriceCard"
+      style={{ boxShadow: "rgba(0, 0, 0, 0.15) 0px 5px 15px 0px" }}
+    >
+      <header className="">
+        <h1 className="text-[#343f52]">
+          <sup className="text-xl font-medium">$</sup>{" "}
+          <span className="text-4xl font-bold">{price}</span>
+          <sub className="font-medium text-base">{yr}</sub>{" "}
+        </h1>
+        <h3 className="text-[#343f52] text-xl mt-2 mb-6 font-bold">
+          Premium Plan
+        </h3>
+      </header>
+      <div>
+        <ul className="mb-8">
+          <li className="flex flex-row gap-2 mt-4">
+            <BiCheck className="w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]" />{" "}
+            <span className="font-bold text-black"> {project} </span>Project
+          </li>
+          <li className="flex flex-row gap-2 mt-4">
+            <BiCheck className="w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]" />{" "}
+            <span className="font-bold text-black"> {api} </span>API Access
+          </li>
+          <li className="flex flex-row gap-2 mt-4">
+            <BiCheck className="w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]" />{" "}
+            <span className="font-bold text-black"> {storage} </span>Storage
+          </li>
+          <li className="flex flex-row gap-2 mt-4">
+            <BiCheck className="w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]" />{" "}
+            <span className="font-bold text-black"> Weekly </span>Reports
+          </li>
+          <li className="flex flex-row gap-2 mt-4">
+            <BiCheck className="w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]" />{" "}
+            <span className="font-bold text-black"> 7/24 </span>Support
+          </li>
+        </ul>
+      </div>
+      <Button button="Choose Plan" />
+    </div>
+  );
+};
