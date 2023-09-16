3a43d5e243d6ff45a96d48baff8030515745f169
smshaown <smshaown65@gmail.com>
2023-09-16 06:21:10 +0600

    Analysis Section data responsive done

--- src/components/PagesSection/Services/Services2/AnalysisSection.jsx
+++ src/components/PagesSection/Services/Services2/AnalysisSection.jsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import img from "../../../../assets/images/Pages/Services/Services2/i8.png";
 
 const AnalysisSection = () => {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const bar = document.getElementById('bar');
+    const bar = document.getElementById("bar");
     bar.style.width = `${progress}%`;
   }, [progress]);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgress(prevProgress => {
+      setProgress((prevProgress) => {
         if (prevProgress < 100) {
           return prevProgress + 1;
         } else {
@@ -24,11 +25,77 @@ const AnalysisSection = () => {
   }, []);
 
   return (
-    <div className="ml-10 mt-10">
-      <h1 className="ml-10 mt-10">Progress Bar</h1>
-      <div className="ml-10 h-3 relative max-w-xl rounded-full overflow-hidden">
-        <div className="w-full h-full bg-gray-200 absolute"></div>
-        <div id="bar" className="transition-all ease-out duration-1000 h-full bg-green-500 relative"></div>
+    <div className="mb-40">
+      <div className="myContainer">
+        <div className="flex flex-col md:flex-row  gap-12">
+          <div className="w-full lg:w-[60%]">
+            <img
+              className="w-full h-full"
+              src={img}
+              alt="Analysis Section Img"
+            />
+          </div>
+          <div className="w-full lg:w-[40%]">
+            <h1 className="text-4xl md:text-[40px] font-bold text-[#343f52]">
+              We bring rapid solutions to make the life of our customers easier.
+            </h1>
+
+            <div className=" mt-10">
+              <h1 className="mt-10 flex flex-row justify-between items-center text-[#60697b] font-medium">
+                Marketing <span>100%</span>
+              </h1>
+              <div className="mt-2 h-[6px] relative max-w-xl rounded-full overflow-hidden">
+                <div className="w-full h-full bg-gray-200 absolute"></div>
+                <div
+                  id="bar"
+                  className="transition-all ease-out duration-1000 h-full bg-[#3f78e0] relative"
+                ></div>
+              </div>
+            </div>
+
+            {/* Strategy progress  */}
+            <div className=" mt-10">
+              <h1 className="mt-10 flex flex-row justify-between items-center text-[#60697b] font-medium">
+                Strategy <span>80%</span>
+              </h1>
+              <div className="mt-2 h-[6px] relative max-w-xl rounded-full overflow-hidden">
+                <div className="w-full h-full bg-gray-200 absolute"></div>
+                <div
+                  id="bar"
+                  className="w-[80%] transition-all ease-out duration-1000 h-full bg-[#FAB758] relative"
+                ></div>
+              </div>
+            </div>
+
+            {/* Development progress  */}
+            <div className=" mt-10">
+              <h1 className="mt-10 flex flex-row justify-between items-center text-[#60697b] font-medium">
+                Development <span>85%</span>
+              </h1>
+              <div className="mt-2 h-[6px] relative max-w-xl rounded-full overflow-hidden">
+                <div className="w-full h-full bg-gray-200 absolute"></div>
+                <div
+                  id="bar"
+                  className="w-[85%] transition-all ease-out duration-1000 h-full bg-[#F78B77] relative"
+                ></div>
+              </div>
+            </div>
+
+            {/* Data Analysis progress  */}
+            <div className=" mt-10">
+              <h1 className="mt-10 flex flex-row justify-between items-center text-[#60697b] font-medium">
+                Data Analysis <span>85%</span>
+              </h1>
+              <div className="mt-2 h-[6px] relative max-w-xl rounded-full overflow-hidden">
+                <div className="w-full h-full bg-gray-200 absolute"></div>
+                <div
+                  id="bar"
+                  className="w-[90%] transition-all ease-out duration-1000 h-full bg-[#45C4A0] relative"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/PagesSection/Services/Services2/PerfectControl.jsx
@@ -0,0 +1,40 @@
+import { BiCheck } from 'react-icons/bi';
+import img from '../../../../assets/images/Pages';
+
+export const PerfectControl = () => {
+  return (
+    <div>
+        <div className="myContainer">
+            <div className="flex flex-col md:flex-row  gap-12">
+                <div className="w-full lg:w-[40%]">
+                  <h1 className="text-4xl md:text-[40px] font-bold text-[#343f52]">
+                    We bring rapid solutions to make the life of our customers easier.
+                  </h1>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia, dolorum. Unde ea quidem exercitationem. Recusandae dicta mollitia, quis libero reiciendis natus sit error est fugit, iusto laborum velit commodi 
+                  </p>
+                  <div>
+                    <div>
+                        < BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' />
+                        <p>Aenean quam ornare. Curabitur blandit</p>
+                    </div>
+                    <div>
+                        < BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' />
+                        <p>Etiam porta euismod malesuada mollis.</p>
+                    </div>
+                    <div>
+                        < BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' /> 
+                        <p>Nullam quis risus eget urna mollis ornare</p> 
+                    </div>
+                    <div>
+                       < BiCheck className='w-6 h-6 rounded-full text-[#3f78e0] bg-[#dce7f9]' />
+                        <p>Vivamus sagittis lacus vel augue rutrum.</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full lg:w-[60%]"></div>
+            </div>
+        </div>
+    </div>
+  )
+}
