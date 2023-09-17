87ef04f9ffe806056d4ac14fe0a11508cf44b069
smshaown <smshaown65@gmail.com>
2023-09-17 23:14:19 +0600

    working steps section done

--- src/components/PagesSection/About/About1/TitleForAbout.jsx
+++ src/components/PagesSection/About/About1/TitleForAbout.jsx
@@ -5,7 +5,7 @@ export const TitleForAbout = ({svg, title}) => {
     <div className='myContainer'>
          <div className='w-full md:w-[600px] mx-auto'>
              <span className="flex flex-col justify-center items-center mb-6">{svg}</span>
-            <h1 className='text-2xl text-center md:text-4xl font-bold text-[#252931]'>{title}</h1>
+            <h1 className='text-2xl text-center md:text-[35px] font-bold text-[#252931] leading-10'>{title}</h1>
         </div>
     </div>
   )
--- src/components/PagesSection/About/About1/WorkingStepsSection.jsx
+++ src/components/PagesSection/About/About1/WorkingStepsSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TitleForAbout } from "./TitleForAbout";
+import { Button } from "../../../../SameSection/Button/Button";
 
 export const WorkingStepsSection = () => {
   return (
@@ -33,22 +34,84 @@ export const WorkingStepsSection = () => {
         />
       </div>
 
-      <div className="flex flex-col lg:flex-row justify-between items-center">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-14 mt-8 md:mt-16">
         <div className="w-full lg:w-1/2 lg:order-first order-last">
-          <h1 className="text-4xl md:text-[39px] font-bold text-[#343f52] mt-8">
-           How It Works?
+          <h1 className="text-4xl md:text-[34px] font-bold text-[#343f52] mt-8">
+            How It Works?
           </h1>
           <p className="pt-[28px] text-[#60697b] text-2xl font-medium tracking-wide">
-           Find out everything you need to know and more about how we create our business process models.
+            Find out everything you need to know and more about how we create
+            our business process models.
           </p>
-          <p className="text-[#60697b] font-light pt-6 tracking-wide leading-6">
-              Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare.
+          <p className="text-[#60697b] text-[17.50px] font-light pt-6 tracking-wide leading-6">
+            Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+            vestibulum. Etiam porta sem malesuada magna mollis euismod. Nullam
+            id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus
+            eget urna mollis ornare.
           </p>
-          <p className="text-[#60697b] font-light pt-6 tracking-wide leading-6">
-             Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed consectetur. Sed posuere consectetur est at lobortis. Vestibulum id ligula porta felis.
+          <p className="text-[#60697b] text-[17.50px] font-light pt-6 tracking-wide leading-6 mb-8">
+            Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id
+            ligula porta felis euismod semper. Aenean lacinia bibendum nulla sed
+            consectetur. Sed posuere consectetur est at lobortis. Vestibulum id
+            ligula porta felis.
           </p>
+
+          <span className="mt-4">
+            <Button button="Learn More" />
+          </span>
         </div>
-        <div className="w-full lg:w-1/2"></div>
+
+        {/* Right Side section  */}
+        <div className="w-full lg:w-1/2 mt-8 md:mt-12">
+          <div className="flex flex-col justify-center items-center">
+
+            <div className="w-full h-full lg:w-[490px] lg:h-[120px] "
+              style={{ boxShadow: "0 0 0 .05rem rgba(8,60,130,.06),0 0 1.25rem rgba(30,34,40,.04)"}}>
+                  <div className="flex flex-row  items-center pt-8 pl-8">
+                     <div className="w-14 h-14 bg-[#E0E9FA] rounded-full flex justify-center items-center mr-5">
+                      <h4 className="font-bold text-xl text-[#3F78E0]">1</h4>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Collect Ideas</h2>
+                      <p className="text-[#60697b] font-light text=lg mt-1">Nulla vitae elit libero pharetra augue dapibus.</p>
+                    </div>
+                </div>
+              </div>
+
+
+
+            <div className="w-full h-full lg:w-[490px] lg:h-[120px] mt-6 lg:ml-12"
+              style={{ boxShadow: "0 0 0 .05rem rgba(8,60,130,.06),0 0 1.25rem rgba(30,34,40,.04)"}}>
+                  <div className="flex flex-row  items-center pt-8 pl-8">
+                     <div className="w-14 h-14 bg-[#E0E9FA] rounded-full flex justify-center items-center mr-5">
+                      <h4 className="font-bold text-xl text-[#3F78E0]">2</h4>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Data Analysis</h2>
+                      <p className="text-[#60697b] font-light text=lg mt-1">Nulla vitae elit libero pharetra augue dapibus.</p>
+                    </div>
+                </div>
+              </div>
+
+
+              
+            <div className="w-full h-full lg:w-[490px] lg:h-[120px] mt-6"
+              style={{ boxShadow: "0 0 0 .05rem rgba(8,60,130,.06),0 0 1.25rem rgba(30,34,40,.04)"}}>
+                  <div className="flex flex-row  items-center pt-8 pl-8">
+                     <div className="w-14 h-14 bg-[#E0E9FA] rounded-full flex justify-center items-center mr-5">
+                      <h4 className="font-bold text-xl text-[#3F78E0]">3</h4>
+                    </div>
+                    <div>
+                      <h2 className="text-xl font-bold">Finalize Product</h2>
+                      <p className="text-[#60697b] font-light text=lg mt-1">Nulla vitae elit libero pharetra augue dapibus.</p>
+                    </div>
+                </div>
+              </div>
+
+
+
+          </div>
+        </div>
       </div>
     </div>
   );
