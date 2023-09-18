cbe64d1f7d192f9dcd45df1f8307691aae91c2c2
smshaown <smshaown65@gmail.com>
2023-09-18 21:30:18 +0600

    start blocks react tabs codeing

---
+++ src/components/BlocksSection/Blocks/Blocks.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+export const Blocks = () => {
+
+    const [activeTab, setActiveTab] = useState(0);
+
+    const handleTabClick = (index) => {
+      setActiveTab(index);
+    }
+  
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <div className="flex border-b">
+        <div
+          className={`mr-2 px-4 py-2 cursor-pointer ${activeTab === 0 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabClick(0)}
+        >
+          Tab 1
+        </div>
+        <div
+          className={`mr-2 px-4 py-2 cursor-pointer ${activeTab === 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabClick(1)}
+        >
+          Tab 2
+        </div>
+        <div
+          className={`px-4 py-2 cursor-pointer ${activeTab === 2 ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+          onClick={() => handleTabClick(2)}
+        >
+          Tab 3
+        </div>
+      </div>
+      <div className="mt-4">
+        {activeTab === 0 && <div>Content for Tab 1</div>}
+        {activeTab === 1 && <div>Content for Tab 2</div>}
+        {activeTab === 2 && <div>Content for Tab 3</div>}
+      </div>
+    </div>
+  )
+}
